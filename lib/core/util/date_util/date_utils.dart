9f3b65af29720f6c0331d670363b5fb5a65341dc
vinod <vinod.sevak@jarvistechnolabs.com>
2024-04-10 17:03:58 +0530

    - added pngs

--- lib/core/util/date_util/date_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:intl/intl.dart';
-
-class DateUtil {
-  static String getMonthDay({required DateTime date}) =>
-      DateFormat('MMM d').format(date);
-
-  static String get24ClockTimeFromDate({required DateTime date}) =>
-      DateFormat("HH:mm").format(date);
-}
